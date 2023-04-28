81dd51f8b78c4127f9108d50f7918a991359ae1e
angel <ANGEL.GARCIA0038@GMAIL.COM>
2023-04-27 21:29:08 -0700

    added a few files for meal details

---
+++ recipeData.js
@@ -0,0 +1,93 @@
+console.log('Youre in')
+
+const title = document.getElementById('dish-name')
+const testRecipe = {
+    vegetarian: true,
+    vegan: false,
+    glutenFree: true,
+    dairyFree: false,
+    veryHealthy: false,
+    cheap: false,
+    veryPopular: false,
+    sustainable: false,
+    lowFodmap: false,
+    weightWatcherSmartPoints: 18,
+    gaps: 'no',
+    preparationMinutes: -1,
+    cookingMinutes: -1,
+    aggregateLikes: 84,
+    healthScore: 21,
+    creditsText: 'Foodista.com – The Cooking Encyclopedia Everyone Can Edit',
+    license: 'CC BY 3.0',
+    sourceName: 'Foodista',
+    pricePerServing: 124.28,
+    extendedIngredients: [
+      {
+        id: 12061,
+        aisle: 'Nuts',
+        image: 'almonds.jpg',
+        consistency: 'SOLID',
+        name: 'almonds',
+        nameClean: 'almonds',
+        original: '100g almonds, finely ground',
+        originalName: 'almonds, finely ground',
+        amount: 100,
+        unit: 'g',
+        meta: ['finely'],
+        measures: {
+          us: {
+            amount: 3.527,
+            unitShort: 'oz',
+            unitLong: 'ounces',
+          },
+          metric: {
+            amount: 100,
+            unitShort: 'g',
+            unitLong: 'grams',
+          },
+        },
+      },
+    ],
+    id: 637016,
+    title: 'Caramel Peanut Fudge Cake',
+    readyInMinutes: 45,
+    servings: 10,
+    sourceUrl: 'http://www.foodista.com/recipe/DFQVM7MF/caramel-peanut-cake',
+    image: 'https://spoonacular.com/recipeImages/637016-556x370.jpg',
+    imageType: 'jpg',
+    summary:
+      "Caramel Peanut Fudge Cake might be just the dessert you are searching for. One portion of this dish contains approximately <b>16g of protein</b>, <b>35g of fat</b>, and a total of <b>459 calories</b>. This recipe serves 10. For <b>$1.24 per serving</b>, this recipe <b>covers 23%</b> of your daily requirements of vitamins and minerals. From preparation to the plate, this recipe takes about <b>45 minutes</b>. 84 people were glad they tried this recipe. If you have almonds, sugar, cream, and a few other ingredients on hand, you can make it. It is brought to you by Foodista. It is a good option if you're following a <b>gluten free and lacto ovo vegetarian</b> diet. Taking all factors into account, this recipe <b>earns a spoonacular score of 74%</b>, which is solid. If you like this recipe, you might also like recipes such as <a href='https://spoonacular.com/recipes/slow-cooker-caramel-peanut-butter-hot-fudge-cake-1331813'>Slow Cooker Caramel Peanut Butter Hot Fudge Cake</a>, <a href='https://spoonacular.com/recipes/slow-cooker-caramel-peanut-butter-hot-fudge-cake-951248'>Slow Cooker Caramel Peanut Butter Hot Fudge Cake</a>, and <a href='https://spoonacular.com/recipes/caramel-peanut-butter-fudge-573829'>Caramel Peanut Butter Fudge</a>.",
+    cuisines: [],
+    dishTypes: ['dessert'],
+    diets: ['gluten free', 'lacto ovo vegetarian'],
+    occasions: [],
+    instructions:
+      '<ol><li>For the sponge, beat egg yolks with sugar for 3-4 minutes until the mixture doubles in volume and becomes pale yellow.</li><li>Whisk the egg whites until soft peaks form.</li><li>Fold the egg whites gently into the egg yolks cream.</li><li>Gently stir in almonds and cocoa.</li><li>Lightly butter and flour a 20 round cake pan, line with parchment paper. Pour in the sponge mixture Bake in preheated oven at 180C for about 20 minutes or until done (the trick with a toothpick).</li><li>Leave the cake to cool completely in the cake pan, then carefully remove it and split into two layers.</li><li>For the caramel cream heat sugar and water over medium heat and cook, stirring occasionally, until the sugar dissolves and comes to a boil. Continue cooking, but without stirring, until mixture becomes golden amber in color. Remove from flame and set aside.</li><li>Whip the cream, gradually stir in the caramel syrup.</li><li>Add peanuts, stir and combine. Mixture must be smooth (at first it will foam up a little). Transfer the cream to a bowl to cool down to room temperature and thicken.</li><li>For the ganache bring the cream just to a boil over medium-high heat; pour over chocolate. Let stand 10 minutes. Stir very gently for 3-4 minutes until smooth and glossy, incorporating the cream steadily, without overworking.</li><li>Cool ganache for an hour or until completely chilled, then beat for 2-3 minutes or until it becomes fluffy and lighter in color. Do not overbeat because it will become too thick and not spreadable.</li><li>To assemble the cake, first sprinkle each cake layer with half of the rum and water syrup.</li><li>Spread the caramel cream over the bottom layer, cover with the top layer (wet side down).</li><li>Immediately spread ganache over top and sides of cake.</li></ol>',
+    analyzedInstructions: [
+      {
+        name: '',
+        steps: [
+          {
+            number: 1,
+            step: 'For the sponge, beat egg yolks with sugar for 3-4 minutes until the mixture doubles in volume and becomes pale yellow.',
+            ingredients: [
+              {
+                id: 1125,
+                name: 'egg yolk',
+                localizedName: 'egg yolk',
+                image: 'egg-yolk.jpg',
+              },
+            ],
+            equipment: [],
+            length: {
+              number: 4,
+              unit: 'minutes',
+            },
+          },
+        ],
+      },
+    ],
+    originalId: null,
+    spoonacularSourceUrl:
+      'https://spoonacular.com/caramel-peanut-fudge-cake-637016',
+  };
