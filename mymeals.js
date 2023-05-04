3fc20a7ce3d662ddefbfe061cec0c5f396a2fa87
ajstyza <124314058+ajstyza@users.noreply.github.com>
2023-05-03 19:44:14 -0700

    added to forloop

--- mymeals.js
+++ mymeals.js
@@ -12,34 +12,75 @@ var requestUrl = `https://api.spoonacular.com/food/menuItems`;
         };
         */
 // creates mule based on user input, then keeps it in local storage.
+
+var mule = document.getElementById('name').value;
 var createButton = document.getElementById('create-btn');
 function addMule() {
-    var mule = document.getElementById('name').value;
-    console.log(mule)
+    
+   // console.log(mule)
     let mules = localStorage.getItem('mules');
     let parseMules = mules ? JSON.parse(mules):[];
     parseMules.push({name: mule,
                     recipes: [],
                     });
     localStorage.setItem('mules', JSON.stringify(parseMules));
-}
+    console.log(parseMules); //expected array response
+    displayMeal(parseMules);
+   // document.getElementById('box1').textContent = mules;
+};
     createButton.addEventListener('click', addMule);
 
-// removes mule meal from list and localStorage
-var removeButton = document.getElementById('remove-btn');
 
-function removeMule() {
-    var userInput = window.prompt("name the mule you would like to remove");
-    var muleEl = document.getElementById('meal1') // this is removing the name input field all together
-   
-    if (userInput === muleEl) {
-    muleEl.remove();
-    localStorage.removeItem(muleEl);
+function displayMeal(parseMules){
+    for (let i = 0; i < parseMules.length; i++) {
+        const MEALS = parseMules[1];
+        var mealName = MEALS.name
+        if(mealName === mule){
+
+        
+       // var recipe = MEALS.recipe;
+       // console.log(mealName, recipe);
+
+       mealEl = document.createElement("li");
+       mealEl.textContent = mealName;
+       // recipe = document.createElement('p');
+      //  recipe.textContent = recipe;
+    document.getElementById('list-item').append(mealEl);
+    // document.getElementById().append(recipe);
+        }
+
+       // document.getElementById('box1').textContent = mealName;
+       // document.getElementById('box2').textContent = recipe;
+
+
+       // document.createElement()
+        
     }
 };
-    removeButton.addEventListener('click', removeMule)
 
     
+// removes mule meal from list and localStorage
+var removeButton = document.getElementById('remove-btn');
+/*
+function removeMule() {
+    var userInput = window.prompt("name the mule you would like to remove");
+    var muleEl = document.getElementsById('box2'); 
+   
+    if (userInput === muleEl) {
+    alert("NO MULES WERE HARMED DURING REMOVAL...");
+    document.remove(muleEl);
+    localStorage.removeItem('mule');
+    } 
+    else if (userInput !== muleEl) {
+    alert("NO MULES RESPONDED...TRY ANOTHER NAME");
+  }
+    else (!userInput) 
+        alert("MULES WILL LAY AROUND EATING IF YOU DON'T HOLLER AT 'EM")
+    };
+
+    removeButton.addEventListener('click', removeMule)
+*/
+/*
 //fetches meal information from localStorage and appends item to list
 function getMeal(){
     for (let i = 0; i < localStorage.length; i++) {
@@ -53,5 +94,5 @@ function getMeal(){
    
     console.log(mealsArray);
 }
-
+*/
 
