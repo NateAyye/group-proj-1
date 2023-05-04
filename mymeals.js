85a25c987a44407d8175efe684226b81d0416992
ajstyza <124314058+ajstyza@users.noreply.github.com>
2023-05-04 09:27:47 -0700

    added link button and fixed function

--- mymeals.js
+++ mymeals.js
@@ -12,38 +12,45 @@ var requestUrl = `https://api.spoonacular.com/food/menuItems`;
         };
         */
 
-//TO DO: list items must appear within a div container, 
-//and list ina column, rather than a row across the screen;
-//also the listed items delete when the page is refreshed
-
+//TO DO: list items must appear within a div container when appended, 
+//and list into a column downward, rather than a row across the screen;
+//also the listed items delete when the page is refreshed. 
 
 // creates mule based on user input, then keeps it in local storage.
 var createButton = document.getElementById('create-btn');
 function addMule() {
     var mule = document.getElementById('name').value;
+    if(!mule){
+        window.alert("NAME YOUR MULE TO CONTINUE");
+        return
+        
+    }
+    else {
    // console.log(mule)
-    let mules = localStorage.getItem('mules');
-    let parseMules = mules ? JSON.parse(mules):[];
-    parseMules.push({name: mule,
+        let mules = localStorage.getItem('mules');
+        let parseMules = mules ? JSON.parse(mules):[];
+            parseMules.push({name: mule,
                     recipes: [],
                     });
-    localStorage.setItem('mules', JSON.stringify(parseMules));
-    console.log(parseMules); //expected array response
-    displayMeal(parseMules);
-   
+        localStorage.setItem('mules', JSON.stringify(parseMules));
+        //sessionStorage.setItem('mules', JSON.strongify(parseMules));
+        console.log(parseMules); //expect array in console
+        displayMeal(parseMules);
+        } 
 };
     createButton.addEventListener('click', addMule);
-
 
 //displays user input (mule names) as a list in browser
 function displayMeal(parseMules){
         const index = parseMules.length -1;
-        var mealName = parseMules[index].name;
-        console.log(index);
+        var muleName = parseMules[index].name;
+            console.log(index);
 
         mealEl = document.createElement("li");
-        mealEl.textContent = mealName;
-        document.getElementById('list-items').append(mealName);
+        mealEl.textContent = muleName;
+            document.getElementById('list-items').append(muleName);
+            window.alert(muleName + " is ready to be packed. Pack your mule with recipes on the recipes page");
+    
         };
 
     
@@ -52,19 +59,20 @@ var removeButton = document.getElementById('remove-btn');
 
 function removeMule() {
     var userInput = window.prompt("name the mule you would like to remove");
-    var muleEl = document.getElementsById('list-items'); 
+    //var muleName = document.getElementsById('list-items'); 
    
-    if (userInput === muleEl) {
-    alert("NO MULES WERE HARMED DURING REMOVAL...");
-    document.remove(muleEl);
-    localStorage.removeItem('mule');
+    if (userInput === muleName) {
+        
+        window.alert("NO MULES WERE HARMED DURING REMOVAL...");
+        document.getElementById('list-items').remove(muleName);
+        localStorage.removeItem('mule');
     } 
-    else if (userInput !== muleEl) {
-    alert("NO MULES RESPONDED...TRY ANOTHER NAME");
+    else if (userInput !== muleName) {
+        window.alert("NO MULES RESPONDED...TRY ANOTHER NAME");
   }
     else (!userInput) 
-        alert("MULES WILL LAY AROUND EATING IF YOU DON'T HOLLER AT 'EM")
+        window.alert("MULES WILL LAY AROUND EATING IF YOU DON'T HOLLER AT 'EM")
     };
 
-    removeButton.addEventListener('click', removeMule)
+        removeButton.addEventListener('click', removeMule)
 
