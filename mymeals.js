78817fa3ac598c8ae7585d95488d3bbc3ceaf08e
ajstyza <124314058+ajstyza@users.noreply.github.com>
2023-05-04 14:50:34 -0700

    turned mule into create div function

--- mymeals.js
+++ mymeals.js
@@ -12,9 +12,7 @@ var requestUrl = `https://api.spoonacular.com/food/menuItems`;
         };
         */
 
-//TO DO: list items must appear within a div container when appended, 
-//and list into a column downward, rather than a row across the screen;
-//also the listed items delete when the page is refreshed. 
+//TO DO: listed items delete when the page is refreshed--prolly need sessionStorage 
 
 // creates mule based on user input, then keeps it in local storage.
 var createButton = document.getElementById('create-btn');
@@ -35,31 +33,33 @@ function addMule() {
         localStorage.setItem('mules', JSON.stringify(parseMules));
         //sessionStorage.setItem('mules', JSON.strongify(parseMules));
         console.log(parseMules); //expect array in console
-        displayMeal(parseMules);
-        } 
+        displayMealContainer(parseMules);
+    
+}
 };
     createButton.addEventListener('click', addMule);
 
 //displays user input (mule names) as a list in browser
-function displayMeal(parseMules){
+function displayMealContainer(parseMules){
         const index = parseMules.length -1;
         var muleName = parseMules[index].name;
             console.log(index);
 
-        mealEl = document.createElement("li");
-        mealEl.textContent = muleName;
-            document.getElementById('list-items').append(muleName);
-            window.alert(muleName + " is ready to be packed. Pack your mule with recipes on the recipes page");
-    
-        };
+        const mealContainer = document.createElement('div');
+        mealContainer.setAttribute('id', 'meal-box');
+        mealContainer.textContent = muleName;
+        document.querySelector('.output').appendChild(mealContainer);
+        window.alert(muleName + " is ready to be packed. Pack your mule with recipes on the recipes page");
+        
+ };
 
-    
+
 // removes mule meal from list and localStorage
 var removeButton = document.getElementById('remove-btn');
 
 function removeMule() {
     var userInput = window.prompt("name the mule you would like to remove");
-    //var muleName = document.getElementsById('list-items'); 
+    var muleName = querySelector(".meal-box")
    
     if (userInput === muleName) {
         
@@ -69,10 +69,9 @@ function removeMule() {
     } 
     else if (userInput !== muleName) {
         window.alert("NO MULES RESPONDED...TRY ANOTHER NAME");
-  }
+    }
     else (!userInput) 
         window.alert("MULES WILL LAY AROUND EATING IF YOU DON'T HOLLER AT 'EM")
-    };
-
+};
+    
         removeButton.addEventListener('click', removeMule)
-
